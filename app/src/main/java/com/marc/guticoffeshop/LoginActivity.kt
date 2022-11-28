84b69c256e1e3f6c47eb56d53e89dd4a836513c0
MarcGuti45 <marc.gutierrez@institutvidreres.cat>
2022-11-28 11:55:09 +0100

    Splash Activity

--- app/src/main/java/com/marc/guticoffeshop/LoginActivity.kt
+++ app/src/main/java/com/marc/guticoffeshop/LoginActivity.kt
@@ -1,11 +1,26 @@
 package com.marc.guticoffeshop
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val buttonEntrar = findViewById<Button>(R.id.buttonEntrar)
+        val buttonCrear = findViewById<Button>(R.id.buttonCrear)
+
+        buttonEntrar.setOnClickListener(){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonCrear.setOnClickListener(){
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/marc/guticoffeshop/SplashActivity.kt
+++ app/src/main/java/com/marc/guticoffeshop/SplashActivity.kt
@@ -3,12 +3,10 @@ package com.marc.guticoffeshop
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.CountDownTimer
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        startActivity(Intent(this, LoginActivity::class.java))
     }
 }
