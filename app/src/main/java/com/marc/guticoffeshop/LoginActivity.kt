50c5a00531914a861b84c3a50eb61d1f41e3219e
MarcGuti45 <marc.gutierrez@institutvidreres.cat>
2023-01-17 19:29:08 +0100

    Login pocho

--- app/src/main/java/com/marc/guticoffeshop/LoginActivity.kt
+++ app/src/main/java/com/marc/guticoffeshop/LoginActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,10 +14,16 @@ class LoginActivity : AppCompatActivity() {
 
         val buttonEntrar = findViewById<Button>(R.id.buttonEntrar)
         val buttonCrear = findViewById<Button>(R.id.buttonCrear)
+        val nomAdmin = findViewById<EditText>(R.id.editTextNombre)
+        val password = findViewById<EditText>(R.id.editTextContraseña)
 
         buttonEntrar.setOnClickListener(){
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            if(nomAdmin.getText().toString() == "Marc" && password.getText().toString() == "admin123") {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else{
+                Toast.makeText(this, "Nom d'admin o password incorrecte, torna a provar", Toast.LENGTH_SHORT).show()
+            }
         }
 
         buttonCrear.setOnClickListener(){
